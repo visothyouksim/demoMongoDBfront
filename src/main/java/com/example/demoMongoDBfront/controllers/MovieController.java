df39c5e032ab66aab7d6d9518fd1c484beccd612
Visoth YOUK-SIM <115548934+visothyouksim@users.noreply.github.com>
2024-06-07 09:26:39 +0200

    affichage allMovies OK

--- src/main/java/com/example/demoMongoDBfront/controllers/MovieController.java
+++ src/main/java/com/example/demoMongoDBfront/controllers/MovieController.java
@@ -1,4 +1,30 @@
 package com.example.demoMongoDBfront.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestClient;
+
+import com.example.demoMongoDBfront.models.Movie;
+import com.example.demoMongoDBfront.services.MovieService;
+
+@Controller
 public class MovieController {
+
+    @Autowired
+    MovieService movieService;
+
+    @Autowired
+    RestClient.Builder restClientBuilder;
+
+    @GetMapping("/movies")
+    public String allMovies(Model model) {
+        List<Movie> movie = movieService.getMovies("movies");
+        model.addAttribute("movies", movie);
+        return "movies";
+    }
+
 }
