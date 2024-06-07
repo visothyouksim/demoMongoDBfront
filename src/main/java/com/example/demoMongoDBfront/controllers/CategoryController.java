a44083ee596bb0beff50aef21e231fcca7f41e17
Visoth YOUK-SIM <115548934+visothyouksim@users.noreply.github.com>
2024-06-07 15:11:41 +0200

    template movie ID

--- src/main/java/com/example/demoMongoDBfront/controllers/CategoryController.java
+++ src/main/java/com/example/demoMongoDBfront/controllers/CategoryController.java
@@ -2,7 +2,6 @@ package com.example.demoMongoDBfront.controllers;
 
 import java.util.List;
 
-import com.example.demoMongoDBfront.model.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
